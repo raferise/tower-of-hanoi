e14a26ddd2e835c0d88316d008592355e335488c
Riley Ferise <raferise@gmail.com>
2020-12-17 16:23:59 -0500

    completed game logic

--- code.js
+++ code.js
@@ -3,7 +3,7 @@
 // 2. You can only put a ring on a ring that is bigger (or an empty pole)
 
 //HOW TO PLAY THE GAME
-// 1.  player chooses a ring
+// 1.  player chooses a ring 
 // 1.a check the rules before allowing the ring to be chosen
 // 2.  player chooses a pole
 // 2.a check the rules before the ring moves
@@ -16,18 +16,37 @@
 class Tower {
     constructor(htmlTowerElement) {
         this.htmlTowerElement = htmlTowerElement;
-        this.isGoalPole = '';
-        //bind onTowerClick
+        //whether or not htmlTowerElement has the class "goal"
+        this.isGoalPole = htmlTowerElement.classList.contains("goal");
+        //adds a click event listner to onTowerClick. bind(this) makes the 'this' keyword in the callback refer to this object.
+        htmlTowerElement.addEventListener("click", this.onTowerClick.bind(this));
     }
     onTowerClick() {
-        //if a ring is not yet selected (document.querySelector(".ring.selected"))
-            //set .selected class on myself and my topRing
-        //else (ring is already selected)
-            //if the selected ring is my top ring
+        //if a ring is not yet selected
+        //document.querySelector(".ring.selected") --> returns either null or the currently selected ring
+        let selectedRing = document.querySelector(".ring.selected");
+        let topRing = this.getTopRing();
+        if (selectedRing === null) {
+            //add "selected" class to htmlTowerElement and the topRing
+            // this.htmlTowerElement.classList.add("selected");
+            topRing.classList.add('selected');
+        } else { //ring is already selected
+            if (selectedRing === topRing) {//if the selected ring is my top ring
                 //deselect it
-            //else if the selected ring is smaller than my top ring
-                //move the selected ring to my tower
+                selectedRing.classList.remove("selected");
+            //else if the selected ring is smaller than my top ring OR the tower is empty
+            } else if ((this.htmlTowerElement.childElementCount === 1) || (selectedRing.getAttribute("ringsize") < topRing.getAttribute('ringsize'))) {
+                //move the selected ring to my tower, inserting it BEFORE the pole at the end.
+                // console.log(this.htmlTowerElement.lastChildElement);
+                this.htmlTowerElement.insertBefore(selectedRing, this.htmlTowerElement.children[this.htmlTowerElement.childElementCount-1]);
+                selectedRing.classList.remove("selected");
                 //if I am goal pole and I am complete, call playerWin()
+                if (this.isGoalPole && this.isComplete()) {
+                    playerWin();
+                }
+            }
+        }
+        
     }
     getTopRing() {
         //returns the topmost ring element from this tower
@@ -45,6 +64,7 @@ class Tower {
         //returns a boolean stating whether or not I am a complete tower
             //this occurs when I have 4 rings in ascending order
             //since rings can only be placed in ascending order, all that matters is that I have 4 rings.
+        return (this.htmlTowerElement.childElementCount === 5);
     }
 
     //bonus functions for visual appeal, don't worry about them yet :)
@@ -67,7 +87,10 @@ class Tower {
 
 //GAME CONTROLLER
 //Make 3 tower objects and store them.
-let towers;
+let towers = [];
+document.querySelectorAll(".tower").forEach(element => {
+    towers.push(new Tower(element));
+});
 //no global variables will be necessary! I did some thinking and we can actually store gamestate AND the selected ring
 //INSIDE the HTML and with a SINGLE VALUE!!!
 //We can do this by applying the .selected class to a ring. This not only gives us a way to find the selected ring with .querySelector
